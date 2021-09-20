bec24ade4d327b9325270a55dc864d8d84cb0703
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-20 20:53:03 -0300

    refactor: change show user to handle error

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -7,10 +7,14 @@ class ShowUserProfileController {
 
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params;
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
 
-    const user = this.showUserProfileUseCase.execute({ user_id });
-
-    return response.status(200).json(user);
+      return response.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      return response.status(404).json({ error: "User not found!" });
+    }
   }
 }
 
